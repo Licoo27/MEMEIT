a194187416155c40cbe707822caec180e1a76553
AdrienDek <146063090+AdrienDek@users.noreply.github.com>
2024-04-26 12:58:50 +0200

    ajout d'une barre de text
    la barre de text mene vers une autre page, qui bug, il faut regler cela pour que le text que j'ecrit soir sur l'image

--- mon_app.py
+++ mon_app.py
@@ -15,6 +15,17 @@ def image():
   print(image_url)
   return render_template("index2.html", image_url=image_url)
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Récupérer le contenu du formulaire
+        texte_saisi = request.form['texte_saisi']
+        # Faire quelque chose avec le texte saisi, comme l'afficher
+        return f"Vous avez saisi : {texte_saisi}"
+    # Afficher le formulaire
+    return render_template('index.html')
 
 if __name__ == '__main__':
   app.run(debug=True)
+
+
