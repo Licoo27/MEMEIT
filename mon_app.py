bef7b5c645ad48a37ac5fc2c70000ddc2a24e6df
Licoo <46717842+Licoo27@users.noreply.github.com>
2024-05-09 23:05:02 +0200

    Update mon_app.py

--- mon_app.py
+++ mon_app.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for
-
+from flask import Flask, render_template, request, redirect, url_for, session, send_file
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import os
 
 app = Flask(__name__)
-url = ''
+app.secret_key = 'secret_key'
 
 
 @app.route('/')
@@ -10,33 +13,50 @@ def exe():
   return render_template("index.html")
 
 
-@app.route('/index2', methods=['GET' or 'POST'])
+@app.route('/index2', methods=['GET', 'POST'])
 def image():
-  image_url = request.args.get('image_url')
-  print(image_url)
+  if request.method == 'POST':
+    session['image_url'] = request.form['image_url']
+    image_url = session['image_url']
   return render_template("index2.html", image_url=image_url)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        texte_saisi = request.form['texte_saisi']
-        return f"Vous avez saisi : {texte_saisi}"
-    return render_template('index.html')
-  
-  
+
+@app.route('/index3', methods=['GET', 'POST'])
+def texte():
+  if request.method == 'POST':
+    texte_saisi = request.form['texte_saisi']
+    image_url = session.get('image_url', None)
+  return render_template('index3.html',
+                         texte_saisi=texte_saisi,
+                         image_url=image_url)
+
+
 @app.route('/gallery', methods=['GET', 'POST'])
 def gallery():
   return render_template("gallery.html")
 
 
+def take_screenshot(url):
+  options = webdriver.ChromeOptions()
+  options.add_argument('--headless')  # Exécuter Chrome en arrière-plan
+  options.add_argument('--no-sandbox')  # Pour éviter les erreurs de sandbox
+  driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+  driver.get(url)
+  # Attendre que la page se charge complètement (vous pouvez ajuster le délai si nécessaire)
+  time.sleep(2)
+  # Prendre un screenshot de la page entière
+  screenshot_path = 'screenshot.jpg'
+  driver.save_screenshot(screenshot_path)
+  driver.quit()
+  return screenshot_path
+
+
 @app.route('/sauvegarder', methods=['POST'])
 def sauvegarder():
-    # logique pour sauvegarder les données
-    # Rediriger vers une autre page
-    return redirect(url_for('gallery'))
+  url = request.url_root + 'index3'  # URL de la page à capturer
+  screenshot_path = take_screenshot(url)  # Prendre le screenshot de la page
+  return send_file(screenshot_path, as_attachment=True)
 
 
 if __name__ == '__main__':
   app.run(debug=True)
-
-
