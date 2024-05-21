ae2bb282e42f7d3ae55618cdacec43e143a8508f
Gatsby4444 <163290728+Gatsby4444@users.noreply.github.com>
2024-05-21 20:35:21 +0200

    Update mon_app.py
    gestion des textes de index2
    suppression des dépendances inutiles

--- mon_app.py
+++ mon_app.py
@@ -1,32 +1,27 @@
-from flask import Flask, render_template, request, redirect, url_for, session, send_file
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-import time
-import os
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
 app.secret_key = 'secret_key'
 
-
 @app.route('/')
 def exe():
-  return render_template("index.html")
-
+    return render_template("index.html")
 
 @app.route('/index2', methods=['GET', 'POST'])
 def image():
-  if request.method == 'POST':
-    session['image_url'] = request.form['image_url']
-    image_url = session['image_url']
-  return render_template("index2.html", image_url=image_url)
-
+    if request.method == 'POST':
+        session['image_url'] = request.form['image_url']
+    return render_template("index2.html", image_url=session.get('image_url'))
 
 @app.route('/index3', methods=['GET', 'POST'])
 def texte():
-  if request.method == 'POST':
-    texte_saisi = request.form['texte_saisi']
-    image_url = session.get('image_url', None)
-  return render_template('index3.html', texte_saisi=texte_saisi, image_url=image_url)
+    if request.method == 'POST':
+        session['texte_saisi1'] = request.form['texte_saisi1']
+        session['texte_saisi2'] = request.form['texte_saisi2']
+    return render_template('index3.html', 
+                           texte_saisi1=session.get('texte_saisi1'), 
+                           texte_saisi2=session.get('texte_saisi2'), 
+                           image_url=session.get('image_url'))
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
